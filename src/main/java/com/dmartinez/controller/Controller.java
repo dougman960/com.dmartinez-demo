922cde9cf9a806599e40bee10fef5c182b1ec436
dougman960 <dougman960@gmail.com>
2024-08-07 02:30:03 +0200

    se eliminan clausalas try catch y se simplifica

--- src/main/java/com/dmartinez/controller/Controller.java
+++ src/main/java/com/dmartinez/controller/Controller.java
@@ -1,17 +1,13 @@
 package com.dmartinez.controller;
 
 
-import com.dmartinez.bussinessexception.BusinessException;
 import com.dmartinez.models.dto.BasicResponseDTO;
 import com.dmartinez.models.dto.PricesDTO;
 import com.dmartinez.service.impl.PriceServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,7 +22,6 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/service/proof")
 public class Controller {
-    private static final Logger logger = LogManager.getLogger(Controller.class);
 
     private final PriceServiceImpl priceServiceImpl;
 
@@ -44,17 +39,7 @@ public class Controller {
     public ResponseEntity<PricesDTO> findOffers (@RequestParam(name = "date", required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date,
                                                  @RequestParam(name = "productId", required = true) String productId,
                                                  @RequestParam(name = "shop", required = true) String shop){
-        PricesDTO pricesDTO;
-        try{
-            pricesDTO = priceServiceImpl.findByBrandidAndProductidAndAndDate(shop,productId,date);
-        }catch (DataAccessException e){
-            logger.error(e.getMessage() + (": ") + (e.getMostSpecificCause().getMessage()));
-            throw new BusinessException(HttpStatus.INTERNAL_SERVER_ERROR, "Error when making requests to the database");
-        }catch (Exception ex){
-            logger.info(ex.getMessage());
-            throw new BusinessException(HttpStatus.NOT_FOUND, ex.getMessage());
-        }
-        return  new ResponseEntity<>(pricesDTO, HttpStatus.OK);
+        PricesDTO pricesDTO = priceServiceImpl.findByBrandIdAndProductIdAndDate(shop, productId, date);
+        return new ResponseEntity<>(pricesDTO, HttpStatus.OK);
     }
-
 }
