c54ae41f7536ce9de39716bbc36e5f52dab779f9
dougman960 <dougman960@gmail.com>
2024-08-07 02:27:50 +0200

    se anade control de exception

--- src/main/java/com/dmartinez/controller/ControllerExceptions.java
+++ src/main/java/com/dmartinez/controller/ControllerExceptions.java
@@ -4,6 +4,10 @@ package com.dmartinez.controller;
 
 import com.dmartinez.bussinessexception.BusinessException;
 import com.dmartinez.models.dto.BasicResponseDTO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,11 +15,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerExceptions {
 
-    @ExceptionHandler(value = BusinessException.class)
+    private static final Logger logger = LogManager.getLogger(ControllerExceptions.class);
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<BasicResponseDTO> handleDataAccessException(DataAccessException e) {
+        logger.error(e.getMostSpecificCause().getMessage());
+        BasicResponseDTO response = new BasicResponseDTO("Error when making requests to the database");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BusinessException.class)
     public ResponseEntity<BasicResponseDTO> exception(BusinessException ex) {
         BasicResponseDTO basicResponseDto = new BasicResponseDTO();
-        basicResponseDto.setResponse(ex.getMessage().trim());
-        return new ResponseEntity<>(basicResponseDto, ex.getStatus());
+        logger.error(ex.getMessage());
+        basicResponseDto.setResponse(ex.getMessage());
+        return new ResponseEntity<>(basicResponseDto, HttpStatus.NOT_FOUND);
     }
 
 }
